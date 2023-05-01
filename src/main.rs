c22267d55945cd3f9f47629d2f6ab1862e0158d9
Riley1101 <www.riley.s.eileen@gmail.com>
2023-05-01 13:56:26 -0400

    404 page added

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 use std::{
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}, fs, fmt::format,
+    net::{TcpListener, TcpStream}, fs,
 };
 
 fn handle_connection(mut stream:TcpStream){
     let buf_reader = BufReader::new(&mut stream);
-    let http_request : Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    println!("{:#?}",http_request);
-    let request_line = "GET / HTTP/1.1";
+    let request_line = buf_reader.lines().next().unwrap().unwrap(); 
     if request_line == "GET / HTTP/1.1" {
         let status_line= "HTTP/1.1 200 OK";
         let contents = fs::read_to_string("hello.html").unwrap();
